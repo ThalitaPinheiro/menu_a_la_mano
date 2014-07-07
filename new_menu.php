1073869d641dc6847e0100124dcb6a998f13d176
Daniele Jane Cruz <danielejcruz@gmail.com>
2014-07-07 16:40:42 -0300

    Novo Menu
    Ajustado quase igual o anterior so com <p></p>

--- new_menu.php
+++ new_menu.php
@@ -6,27 +6,27 @@
 
 
 <form  method="POST">
-    Tipo do cardapio: 
+    <p>Tipo do cardapio: 
     <select name="tipo_cardapio">
         <option value="a_la_carte">A la Carte</option>
         <option value="executivo">Executivo</option>
         <option value="executivo_especial">Executivo Especial</option>
         <option value="sugestao_chefe">Sugestão do Chefe</option>
-    </select>
-    Data de entrada (exceto A La Carte): <input type="date" name="data_entrada">
-    Data de saída (exceto A La Carte): <input type="date" name="data_saida">
-    Preço (exceto A La Carte): <input type="number" name="preco" min="0" step="0.01">
+    </select></p>
+    <p>Data de entrada (exceto A La Carte): <input type="date" name="data_entrada"></p> 
+    <p>Data de saída (exceto A La Carte): <input type="date" name="data_saida"></p> 
+    <p>Preço (exceto A La Carte): <input type="number" name="preco" min="0" step="0.01"></p> 
     
-    Adicionar Item
-    Nome do prato: <input type="text" name="prato">
-    Categoria:
+    <p>Adicionar Item</p> 
+    <p>Nome do prato: <input type="text" name="prato"></p> 
+    <p>Categoria:
     <select name="tipo_prato">
         <option value="entradas">Entradas</option>
         <option value="risotos">Risotos</option>
         <option value="massas">Massas</option>
         <option value="etc">Etc</option>
-    </select>                      
-    Descrição: <input type="text" name="descricao">
+    </select></p>                       
+    <p>Descrição: <input type="text" name="descricao"></p> 
     
 </form>
 
