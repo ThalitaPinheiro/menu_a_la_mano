d3d9accfd4faa2a650c923033e7789d5094f56af
Daniele Jane Cruz <danielejcruz@gmail.com>
2014-07-07 16:36:53 -0300

    Novo Menu
    Exemplo de como seria o formulário para criar um "cardápio" novo.

--- new_menu.php
+++ new_menu.php
@@ -1,14 +1,33 @@
 <html>
  <head>
-  <title>PHP Teste</title>
+  <title>Novo Cardápio</title>
  </head>
  <body>
 
 
 <form  method="POST">
- Seu nome <input type="text" name="nome" />
- Sua idade: <input type="text" name="idade" />
- <input type="submit">
+    Tipo do cardapio: 
+    <select name="tipo_cardapio">
+        <option value="a_la_carte">A la Carte</option>
+        <option value="executivo">Executivo</option>
+        <option value="executivo_especial">Executivo Especial</option>
+        <option value="sugestao_chefe">Sugestão do Chefe</option>
+    </select>
+    Data de entrada (exceto A La Carte): <input type="date" name="data_entrada">
+    Data de saída (exceto A La Carte): <input type="date" name="data_saida">
+    Preço (exceto A La Carte): <input type="number" name="preco" min="0" step="0.01">
+    
+    Adicionar Item
+    Nome do prato: <input type="text" name="prato">
+    Categoria:
+    <select name="tipo_prato">
+        <option value="entradas">Entradas</option>
+        <option value="risotos">Risotos</option>
+        <option value="massas">Massas</option>
+        <option value="etc">Etc</option>
+    </select>                      
+    Descrição: <input type="text" name="descricao">
+    
 </form>
 
 
