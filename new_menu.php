3c5b030db88f2ecebd2d0b3f6fe2000bc698a4d0
Daniele Jane Cruz <danielejcruz@gmail.com>
2014-07-15 14:52:27 -0300

    Javascript
    desabilitando os campos com javascript
    outra opção é incluir os campos depois de selecionado o tipo de cardapio

--- new_menu.php
+++ new_menu.php
@@ -1,5 +1,18 @@
 <html>
  <head>
+     <script>
+        function menu_type(type) {
+            if(type!="a_la_carte") {
+                document.getElementById("preco").disabled = false;
+                document.getElementById("data_entrada").disabled = false;
+                document.getElementById("data_saida").disabled = false;
+            } else {
+                document.getElementById("preco").disabled = true;
+                document.getElementById("data_entrada").disabled = true;
+                document.getElementById("data_saida").disabled = true;
+            }
+        }
+     </script>
   <title>Novo Cardápio</title>
  </head>
  <body>
@@ -15,7 +28,7 @@
             <td><strong>Tipo do card&aacute;pio:  </strong></td>
             
             <td>
-                <select name="tipo_cardapio">
+                <select name="tipo_cardapio" onchange="menu_type(document.forms[0].tipo_cardapio.value)">
                     <option value="a_la_carte">A la Carte</option>
                     <option value="executivo">Executivo</option>
                     <option value="executivo_especial">Executivo Especial</option>
@@ -24,26 +37,19 @@
             </td>
         </tr>
 
-        <?php
-            if ($tipo_cardapio!="a_la_carte") {
-                echo "teste";
-            }
-           
-        ?>
-
         <tr>
             <td><strong>Data de entrada (exceto A La Carte): </strong></td>
-            <td><input type="date" name="data_entrada"></td>
+            <td><input type="date" name="data_entrada" id="data_entrada" disabled></td>
         </tr>
 
         <tr>
             <td><strong> Data de saída (exceto A La Carte): </strong></td>
-            <td><input type="date" name="data_saida"></td>
+            <td><input type="date" name="data_saida" id="data_saida" disabled></td>
         </tr>
             
         <tr>
             <td><strong>Preço (exceto A La Carte): </strong></td>
-            <td><strong> <input type="number" name="preco" min="0" step="0.01"></td>
+            <td><strong> <input type="number" name="preco"  id="preco" min="0" step="0.01" disabled></td>
         </tr>
 
         <tr>
