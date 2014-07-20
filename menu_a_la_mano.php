13649ad4c3c2f3f58e6a8706b00d5710e89ed226
Daniele Jane Cruz <danielejcruz@gmail.com>
2014-07-20 13:50:18 -0300

    Campo preço do Vinho

--- menu_a_la_mano.php
+++ menu_a_la_mano.php
@@ -37,14 +37,17 @@
             public function __construct()
             {
                 //MENU
-                add_action('admin_menu', array(&$this, 'menu'));
+                add_action('admin_menu', array($this, 'menu'));
                 // Hook into the 'init' action
                 
-                add_action( 'init', array(&$this, 'wine_taxonomy'), 0 );
+                add_action( 'init', array($this, 'wine_taxonomy'), 0 );
                 
-                add_action( 'init', array(&$this, 'create_posttype') );
+                add_action( 'init', array($this, 'wine_post_type'), 0 );
   
-
+                //Adiciona campo personalizado
+                add_action("admin_init", array($this, 'admin_init_wine'));
+                add_action('save_post', array($this, 'save_price_wine'));
+            
                 // register actions
             } // END public function __construct
     
@@ -72,23 +75,22 @@
             
             // Registra tipos de vinho
             function wine_taxonomy() {
-            
                 $labels = array(
-                    'name'                       => _x( 'Categorias de Vinhos', 'Taxonomy General Name', 'text_domain' ),
-                    'singular_name'              => _x( 'Categoria de Vinho', 'Taxonomy Singular Name', 'text_domain' ),
-                    'menu_name'                  => __( 'Categoria de Vinho', 'text_domain' ),
-                    'all_items'                  => __( 'Todos as Categorias de Vinho', 'text_domain' ),
-                    'parent_item'                => __( 'Parent Item', 'text_domain' ),
-                    'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-                    'new_item_name'              => __( 'Nome de Nova Categoria', 'text_domain' ),
-                    'add_new_item'               => __( 'Adicionar Nova Categoria', 'text_domain' ),
-                    'edit_item'                  => __( 'Editar Categoria', 'text_domain' ),
-                    'update_item'                => __( 'Atualizar Categoria', 'text_domain' ),
-                    'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
-                    'search_items'               => __( 'Buscar Categorias', 'text_domain' ),
-                    'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
-                    'choose_from_most_used'      => __( 'Choose from the most used items', 'text_domain' ),
-                    'not_found'                  => __( 'Not Found', 'text_domain' ),
+                    'name'                       => _x( 'Categorias de Vinhos', 'Taxonomy General Name'),
+                    'singular_name'              => _x( 'Categoria de Vinho', 'Taxonomy Singular Name'),
+                    'menu_name'                  => __( 'Categoria de Vinho'),
+                    'all_items'                  => __( 'Todos as Categorias de Vinho'),
+                    'parent_item'                => __( 'Parent Item'),
+                    'parent_item_colon'          => __( 'Parent Item:'),
+                    'new_item_name'              => __( 'Nome de Nova Categoria'),
+                    'add_new_item'               => __( 'Adicionar Nova Categoria'),
+                    'edit_item'                  => __( 'Editar Categoria'),
+                    'update_item'                => __( 'Atualizar Categoria'),
+                    'separate_items_with_commas' => __( 'Separate items with commas'),
+                    'search_items'               => __( 'Buscar Categorias'),
+                    'add_or_remove_items'        => __( 'Adicionar ou remover categorias'),
+                    'choose_from_most_used'      => __( 'Escolher entre as categorias mais usadas'),
+                    'not_found'                  => __( 'Não encontrada'),
                 );
                 $args = array(
                     'labels'                     => $labels,
@@ -99,21 +101,74 @@
                     'show_in_nav_menus'          => true,
                     'show_tagcloud'              => false,
                 );
-                register_taxonomy( 'wine', array( 'carta_vinhos' ), $args );
+                register_taxonomy( 'wine', array( 'wine' ), $args );
             }
             
-            function create_posttype() {
-                register_post_type( 'carta_vinhos',
-                    array(
-                        'labels' => array(
-                            'name' => __( 'Carta de Vinhos' ),
-                            'singular_name' => __( 'Carta de Vinhos' )
-                        ),
-                        'public' => true,
-                        'has_archive' => true,
-                        'rewrite' => array('slug' => 'products'),
-                    )
+            // Define o tipo de post Carta de Vinhos
+            function wine_post_type() {
+                $labels = array(
+                    'name'                => _x( 'Carta de Vinhos', 'Post Type General Name'),
+                    'singular_name'       => _x( 'Carta de Vinhos', 'Post Type Singular Name'),
+                    'menu_name'           => __( 'Carta de Vinhos'),
+                    'parent_item_colon'   => __( 'Parent Item:'),
+                    'all_items'           => __( 'Todos os Itens'),
+                    'view_item'           => __( 'Visualizar Item'),
+                    'add_new_item'        => __( 'Adicionar Novo Item'),
+                    'add_new'             => __( 'Adicionar Novo Vinho'),
+                    'edit_item'           => __( 'Editar Item'),
+                    'update_item'         => __( 'Atualizar Item'),
+                    'search_items'        => __( 'Buscar  Item'),
+                    'not_found'           => __( 'Não Encontrado'),
+                    'not_found_in_trash'  => __( 'Não Encontrado na Lixeira'),
                 );
+                $args = array(
+                    'label'               => __( 'wine'),
+                    'description'         => __( 'Carta de Vinhos'),
+                    'labels'              => $labels,
+                    'supports'            => array( 'title', 'editor'),
+                    'hierarchical'        => true,
+                    'public'              => true,
+                    'show_ui'             => true,
+                    'show_in_menu'        => true,
+                    'show_in_nav_menus'   => true,
+                    'show_in_admin_bar'   => true,
+                    'menu_position'       => 100,
+                    'can_export'          => true,
+                    'has_archive'         => true,
+                    'exclude_from_search' => false,
+                    'publicly_queryable'  => true,
+                    'capability_type'     => 'page',
+                );
+                register_post_type( 'wine', $args );            
+            }
+            
+            function admin_init_wine() {
+                
+                add_meta_box(
+                    'wine_price',       //Nome do metabox
+                    __('Preço'),        //Título da Caixa do campo personalizado
+                    array($this, 'wine_meta'),   //Função que será chamada para exibir o conteúdo
+                    'wine',             //Tipo de post que vai ter esse campo personalizado
+                    'normal',           //Local da página de edição onde será exibido o campo personalizado
+                    'high'              //Prioridade de onde vai aparecer a caixa - high, normal ou low
+                 );
+            }
+            
+            function wine_meta() {
+                global $post;
+                $custom = get_post_custom($post->ID);
+                $price = $custom["price"][0]; // define o campo preço | mesmo nome do input
+?>
+                <p>R$ <input type="text" name="price" id="price" min="0" step="0.01"  value="<?php echo $price; ?>" /></p>
+<?php
+            }
+            
+            //Salva o campo personalizado
+            function save_price_wine() {
+                global $post;
+                // salva os campos personalizados
+                update_post_meta($post->ID, "price", $_POST["price"]);
+                // fim dos campos personalizados
             }
             
         } // END class Menu_a_la_mano
