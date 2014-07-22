d4702b37f6817b5838d2cbac2c636beb6871b8c3
Thalita Nick Pinheiro Gomes <thalitanpg@gmail.com>
2014-07-21 22:21:57 -0300

    comentando includes

--- menu_a_la_mano.php
+++ menu_a_la_mano.php
@@ -62,8 +62,8 @@
             function menu_a_la_mano_includes() {
                 require_once(MENU_A_LA_MANO_DIR . 'wine/wine.php');
                 require_once(MENU_A_LA_MANO_DIR . 'food/food.php');
-                require_once(MENU_A_LA_MANO_DIR . 'template.php');
-                require_once(MENU_A_LA_MANO_DIR . 'pageofposts.php');
+                //require_once(MENU_A_LA_MANO_DIR . 'template.php');
+                //require_once(MENU_A_LA_MANO_DIR . 'pageofposts.php');
                 
             }
             
